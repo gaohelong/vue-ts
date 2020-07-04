0a9601774984c9f08c8543af2477f2c8f17b31c6
versiondk <versiondk@gmail.com>
2020-07-04 15:36:39 +0800

    vue-lazyload

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,19 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
+
+// lazyload
+Vue.use(VueLazyload)
+// or with options
+// Vue.use(VueLazyload, {
+//   preLoad: 1.3,
+//   error: 'dist/error.png',
+//   loading: 'dist/loading.gif',
+//   attempt: 1
+// })
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" v-lazy="vueLogo">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +8,17 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import vueLogo from '@/assets/logo.png'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data: function () {
+    return {
+      vueLogo
+    }
   }
 }
 </script>
