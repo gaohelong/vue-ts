02366ee562adc2634b8ad463c5bde0df774d4a85
versiondk <versiondk@gmail.com>
2020-07-07 18:19:17 +0800

    单元测试: 实例

---
+++ src/components/CptMsg.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <span class="msg">{{ msg }}</span>
+    <button>Increment</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class CptMsg extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  @import './CptTest.scss';
+</style>
--- tests/unit/test.spec.ts
+++ tests/unit/test.spec.ts
@@ -1,12 +1,19 @@
-import { shallowMount } from '@vue/test-utils'
-import CptTest from '@/components/CptTest.vue'
+import { mount, shallowMount } from '@vue/test-utils'
+import CptMsg from '@/components/CptMsg.vue'
 
-describe('CptTest.vue', () => {
-  it('renders props.msg when passed', () => {
+describe('CptMsg.vue', () => {
+  const wrapper = mount(CptMsg)
+
+  it('renders the correct markup', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(CptTest, {
+    const cptWrapper = shallowMount(CptMsg, {
       propsData: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(cptWrapper.html()).toContain('<span class="msg">new message</span>')
+  })
+
+  // 也便于检查已存在的元素
+  it('has a button', () => {
+    expect(wrapper.contains('button')).toBe(true)
   })
 })
